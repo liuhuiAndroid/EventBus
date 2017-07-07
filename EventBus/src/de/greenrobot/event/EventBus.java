3e85862c39ead4d6a36929888dcf7679f6da1cc1
lh <642404044@qq.com>
2017-07-07 14:34:25 +0800

    EventBus2.3.0源码分析

--- EventBus/src/de/greenrobot/event/EventBus.java
+++ EventBus/src/de/greenrobot/event/EventBus.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * EventBus is a central publish/subscribe event system for Android. Events are posted ({@link #post(Object)}) to the
@@ -72,7 +73,11 @@ public class EventBus {
     private final boolean sendNoSubscriberEvent;
     private final boolean eventInheritance;
 
-    /** Convenience singleton for apps using a process-wide EventBus instance. */
+    /**
+     * Convenience singleton for apps using a process-wide EventBus instance.
+     * 单例
+     * 使用了双重判断的方式，防止并发的问题，还能极大的提高效率。
+     */
     public static EventBus getDefault() {
         if (defaultInstance == null) {
             synchronized (EventBus.class) {
@@ -118,6 +123,8 @@ public class EventBus {
         eventInheritance = builder.eventInheritance;
         executorService = builder.executorService;
     }
+
+    // ============================================================================
 
 
     /**
@@ -176,10 +183,26 @@ public class EventBus {
         register(subscriber, methodName, true, 0);
     }
 
+
+    // ====================================================================
+
+    /**
+     * 以上register方法本质上都是调用这个register方法
+     * @param subscriber 是我们扫描类的对象，也就是我们代码中常见的this;
+     * @param methodName 这个是写死的：“onEvent”，用于确定扫描什么开头的方法，可见我们的类中都是以这个开头。
+     * @param sticky 暂时不用管
+     * @param priority 优先级，优先级越高，在调用的时候会越先调用。
+     *
+     * 你只要记得一件事：扫描了所有的方法，把匹配的方法最终保存在subscriptionsByEventType（Map，key：eventType ； value：CopyOnWriteArrayList<Subscription> ）中；
+    eventType是我们方法参数的Class，Subscription中则保存着subscriber, subscriberMethod（method, threadMode, eventType）, priority；包含了执行改方法所需的一切。
+     */
     private synchronized void register(Object subscriber, String methodName, boolean sticky, int priority) {
+        // 调用内部类SubscriberMethodFinder的findSubscriberMethods方法，
+        // 传入了subscriber 的class，以及methodName，返回一个List<SubscriberMethod>。
         List<SubscriberMethod> subscriberMethods = subscriberMethodFinder.findSubscriberMethods(subscriber.getClass(),
                 methodName);
         for (SubscriberMethod subscriberMethod : subscriberMethods) {
+            // for循环扫描到的方法，然后去调用suscribe方法。
             subscribe(subscriber, subscriberMethod, sticky, priority);
         }
     }
@@ -216,6 +239,14 @@ public class EventBus {
         register(subscriber, methodName, true, eventType, moreEventTypes);
     }
 
+    /**
+     * 还有这个不知道干哈的稍后分析
+     * @param subscriber
+     * @param methodName
+     * @param sticky
+     * @param eventType
+     * @param moreEventTypes
+     */
     private synchronized void register(Object subscriber, String methodName, boolean sticky, Class<?> eventType,
                                        Class<?>... moreEventTypes) {
         Class<?> subscriberClass = subscriber.getClass();
@@ -237,11 +268,16 @@ public class EventBus {
 
     // Must be called in synchronized block
     private void subscribe(Object subscriber, SubscriberMethod subscriberMethod, boolean sticky, int priority) {
+        // 根据subscriberMethod.eventType，去subscriptionsByEventType去查找一个CopyOnWriteArrayList<Subscription> ，如果没有则创建。
         Class<?> eventType = subscriberMethod.eventType;
         CopyOnWriteArrayList<Subscription> subscriptions = subscriptionsByEventType.get(eventType);
+        // 顺便把我们的传入的参数封装成了一个：Subscription（subscriber, subscriberMethod, priority）；
         Subscription newSubscription = new Subscription(subscriber, subscriberMethod, priority);
         if (subscriptions == null) {
             subscriptions = new CopyOnWriteArrayList<Subscription>();
+            // 这里的subscriptionsByEventType是个Map，
+            // key：eventType; value：CopyOnWriteArrayList<Subscription>;
+            // 这个Map其实就是EventBus存储方法的地方，一定要记住！
             subscriptionsByEventType.put(eventType, subscriptions);
         } else {
             if (subscriptions.contains(newSubscription)) {
@@ -252,7 +288,7 @@ public class EventBus {
 
         // Starting with EventBus 2.2 we enforced methods to be public (might change with annotations again)
         // subscriberMethod.method.setAccessible(true);
-
+        // 就是添加newSubscription；并且是按照优先级添加的。可以看到，优先级越高，会插到在当前List的前面。
         int size = subscriptions.size();
         for (int i = 0; i <= size; i++) {
             if (i == size || newSubscription.priority > subscriptions.get(i).priority) {
@@ -261,6 +297,8 @@ public class EventBus {
             }
         }
 
+        // 根据subscriber存储它所有的eventType;
+        // 依然是map；key：subscriber ，value：List<eventType> ;知道就行，非核心代码，主要用于isRegister的判断。
         List<Class<?>> subscribedEvents = typesBySubscriber.get(subscriber);
         if (subscribedEvents == null) {
             subscribedEvents = new ArrayList<Class<?>>();
@@ -268,6 +306,8 @@ public class EventBus {
         }
         subscribedEvents.add(eventType);
 
+        // 判断sticky；如果为true，从stickyEvents中根据eventType去查找有没有stickyEvent，如果有则立即发布去执行。
+        // stickyEvent其实就是我们post时的参数。
         if (sticky) {
             Object stickyEvent;
             synchronized (stickyEvents) {
@@ -339,18 +379,27 @@ public class EventBus {
 
     /** Posts the given event to the event bus. */
     public void post(Object event) {
+        // currentPostingThreadState是一个ThreadLocal类型的，
+        // 里面存储了PostingThreadState；PostingThreadState包含了一个eventQueue和一些标志位。
         PostingThreadState postingState = currentPostingThreadState.get();
         List<Object> eventQueue = postingState.eventQueue;
+        // 把我们传入的event，保存到了当前线程中的一个变量PostingThreadState的eventQueue中。
         eventQueue.add(event);
 
+        // isPosting=true了，就不会往下走了,防止多次调用
         if (!postingState.isPosting) {
+            // 判断当前是否是UI线程。
             postingState.isMainThread = Looper.getMainLooper() == Looper.myLooper();
             postingState.isPosting = true;
             if (postingState.canceled) {
                 throw new EventBusException("Internal error. Abort state was not reset");
             }
             try {
+                // 遍历队列中的所有的event，调用postSingleEvent（eventQueue.remove(0), postingState）方法。
+                // 每次post都会去调用整个队列么，那么不会造成方法多次调用么？看上面
                 while (!eventQueue.isEmpty()) {
+                    // 将我们的event，即post传入的实参；以及postingState传入到postSingleEvent中。
+                    // 下面看这里
                     postSingleEvent(eventQueue.remove(0), postingState);
                 }
             } finally {
@@ -387,6 +436,7 @@ public class EventBus {
      * Posts the given event to the event bus and holds on to the event (because it is sticky). The most recent sticky
      * event of an event's type is kept in memory for future access. This can be {@link #registerSticky(Object)} or
      * {@link #getStickyEvent(Class)}.
+     * 和post功能类似，但是会把方法存储到stickyEvents中去；
      */
     public void postSticky(Object event) {
         synchronized (stickyEvents) {
@@ -467,8 +517,12 @@ public class EventBus {
         Class<?> eventClass = event.getClass();
         boolean subscriptionFound = false;
         if (eventInheritance) {
+            // 根据event的Class，去得到一个List<Class<?>>；
+            // 其实就是得到event当前对象的Class，以及父类和接口的Class类型；
+            // 主要用于匹配，比如你传入Dog extends Animal，他会把Animal也装到该List中。
             List<Class<?>> eventTypes = lookupAllEventTypes(eventClass);
             int countTypes = eventTypes.size();
+            // 遍历所有的Class，到subscriptionsByEventType去查找subscriptions；哈哈，熟不熟悉，还记得我们register里面把方法存哪了不？
             for (int h = 0; h < countTypes; h++) {
                 Class<?> clazz = eventTypes.get(h);
                 subscriptionFound |= postSingleEventForEventType(event, postingState, clazz);
@@ -492,12 +546,15 @@ public class EventBus {
         synchronized (this) {
             subscriptions = subscriptionsByEventType.get(eventClass);
         }
+        // 遍历每个subscription,依次去调用postToSubscription(subscription, event, postingState.isMainThread);
+        // 这个方法就是去反射执行方法了，大家还记得在register，if(sticky)时，也会去执行这个方法。
         if (subscriptions != null && !subscriptions.isEmpty()) {
             for (Subscription subscription : subscriptions) {
                 postingState.event = event;
                 postingState.subscription = subscription;
                 boolean aborted = false;
                 try {
+                    // 进去看如何反射执行
                     postToSubscription(subscription, event, postingState.isMainThread);
                     aborted = postingState.canceled;
                 } finally {
@@ -514,12 +571,22 @@ public class EventBus {
         return false;
     }
 
+    /**
+     * subscription包含了所有执行需要的东西，大致有:subscriber, subscriberMethod（method, threadMode, eventType）, priority；
+     * @param subscription
+     * @param event
+     * @param isMainThread
+     */
     private void postToSubscription(Subscription subscription, Object event, boolean isMainThread) {
+        // 第一步根据threadMode去判断应该在哪个线程去执行该方法；
         switch (subscription.subscriberMethod.threadMode) {
             case PostThread:
+                // 直接反射调用；也就是说在当前的线程直接调用该方法；
                 invokeSubscriber(subscription, event);
                 break;
             case MainThread:
+                // 首先去判断当前如果是UI线程，则直接调用；否则： mainThreadPoster.enqueue(subscription, event);
+                // 把当前的方法加入到队列，然后直接通过handler去发送一个消息，在handler的handleMessage中，去执行我们的方法。说白了就是通过Handler去发送消息，然后执行的。
                 if (isMainThread) {
                     invokeSubscriber(subscription, event);
                 } else {
@@ -527,6 +594,8 @@ public class EventBus {
                 }
                 break;
             case BackgroundThread:
+                // 如果当前非UI线程，则直接调用；如果是UI线程，则将任务加入到后台的一个队列，最终由Eventbus中的一个线程池去调用
+                // executorService = Executors.newCachedThreadPool();
                 if (isMainThread) {
                     backgroundPoster.enqueue(subscription, event);
                 } else {
@@ -534,6 +603,10 @@ public class EventBus {
                 }
                 break;
             case Async:
+                // 将任务加入到后台的一个队列，最终由Eventbus中的一个线程池去调用；线程池与BackgroundThread用的是同一个。
+                // 这么说BackgroundThread和Async有什么区别呢？
+                // BackgroundThread中的任务，一个接着一个去调用，中间使用了一个布尔型变量handlerActive进行的控制。
+                // Async则会动态控制并发。
                 asyncPoster.enqueue(subscription, event);
                 break;
             default:
--- EventBus/src/de/greenrobot/event/SubscriberMethodFinder.java
+++ EventBus/src/de/greenrobot/event/SubscriberMethodFinder.java
@@ -15,7 +15,6 @@
  */
 package de.greenrobot.event;
 
-import android.util.Log;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -25,6 +24,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import android.util.Log;
+
 
 class SubscriberMethodFinder {
     /*
@@ -49,6 +51,12 @@ class SubscriberMethodFinder {
         }
     }
 
+    /**
+     * 遍历该类内部所有方法，然后根据methodName去匹配，匹配成功的封装成SubscriberMethod，最后返回一个List。
+     * @param subscriberClass
+     * @param eventMethodName
+     * @return
+     */
     List<SubscriberMethod> findSubscriberMethods(Class<?> subscriberClass, String eventMethodName) {
         String key = subscriberClass.getName() + '.' + eventMethodName;
         List<SubscriberMethod> subscriberMethods;
@@ -70,9 +78,14 @@ class SubscriberMethodFinder {
             }
 
             // Starting with EventBus 2.2 we enforced methods to be public (might change with annotations again)
+            // clazz.getMethods()去得到所有的方法
+            // getMethods(),该方法是获取本类以及父类或者父接口中所有的公共方法(public修饰符修饰的)
+            // getDeclaredMethods(),该方法是获取本类中的所有方法，包括私有的(private、protected、默认以及public)的方法。
             Method[] methods = clazz.getDeclaredMethods();
+            // 就开始遍历每一个方法了，去匹配封装了。
             for (Method method : methods) {
                 String methodName = method.getName();
+                // 分别判断了是否以onEvent开头，是否是public且非static和abstract方法，是否是一个参数。如果都复合，才进入封装的部分。
                 if (methodName.startsWith(eventMethodName)) {
                     int modifiers = method.getModifiers();
                     if ((modifiers & Modifier.PUBLIC) != 0 && (modifiers & MODIFIERS_IGNORE) == 0) {
@@ -80,6 +93,7 @@ class SubscriberMethodFinder {
                         if (parameterTypes.length == 1) {
                             String modifierString = methodName.substring(eventMethodName.length());
                             ThreadMode threadMode;
+                            // 也比较简单，根据方法的后缀，来确定threadMode，threadMode是个枚举类型：就四种情况。
                             if (modifierString.length() == 0) {
                                 threadMode = ThreadMode.PostThread;
                             } else if (modifierString.equals("MainThread")) {
@@ -102,6 +116,8 @@ class SubscriberMethodFinder {
                             String methodKey = methodKeyBuilder.toString();
                             if (eventTypesFound.add(methodKey)) {
                                 // Only add if not already found in a sub class
+                                // 将method, threadMode, eventType传入构造了：new SubscriberMethod(method, threadMode, eventType)。
+                                // 添加到List，最终放回
                                 subscriberMethods.add(new SubscriberMethod(method, threadMode, eventType));
                             }
                         }
@@ -111,6 +127,7 @@ class SubscriberMethodFinder {
                     }
                 }
             }
+            // 可以看到，会扫描所有的父类，不仅仅是当前类。
             clazz = clazz.getSuperclass();
         }
         if (subscriberMethods.isEmpty()) {
